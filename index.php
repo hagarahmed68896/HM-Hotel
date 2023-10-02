345cf39580f18706509c078ae76f80fe82289c25
hagarahmed68896 <76259845+hagarahmed68896@users.noreply.github.com>
2023-10-03 00:26:07 +0200

    add footer

--- index.php
+++ index.php
@@ -560,7 +560,47 @@
 
 
 
-<br><br><br><br><br>
+
+
+
+
+<!-- footer -->
+<div class="container-fluid bg-white mt-5">
+  <div class="row">
+    <div class="col-lg-4 p-4">
+      <h3 class="fs-3 h-font fw-bold mb-2">HM Hotel</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Enim excepturi, animi iure quae explicabo ducimus expedita quos quo quidem,
+        iusto culpa voluptas reprehenderit nobis assumenda.
+        Qui maiores facere aliquid praesentium?
+      </p>
+    </div>
+    <div class="col-lg-4 p-4">
+      <h5 class="mb-3">Links</h5>
+      <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Home</a><br>
+      <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Rooms</a><br>
+      <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Facilities</a><br>
+      <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Contact Us</a><br>
+      <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">About</a>
+
+    </div>
+    <div class="col-lg-4 p-4">
+      <h5 class="mb-3">Follow Us</h5>
+      <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">
+        <i class="bi bi-twitter me-1"></i>  Twitter</a><br>
+      <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">
+          <i class="bi bi-facebook me-1"></i>  Facebook</a><br>
+      <a href="#" class="d-inline-block text-dark text-decoration-none">
+            <i class="bi bi-instagram me-1"></i>  Instagram</a><br>
+    </div>
+  </div>
+</div>
+
+
+<h6 class="text-center bg-dark text-white p-3 m-0">Designed and Developed by Hagar Ahmed WEBDEVE</h6>
+
+
 
 
 <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
