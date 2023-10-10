fad8587a8e7e2e0ed695d6f78cc615a426195b4b
hagarahmed68896 <76259845+hagarahmed68896@users.noreply.github.com>
2023-10-11 01:02:38 +0200

    Cntact settings section in progress

--- admin/settings.php
+++ admin/settings.php
@@ -45,24 +45,24 @@ adminlogin();
             <!-- General settings modal  -->
                 <div class="modal fade" id="general-settings" data-bs-backdrop="static" data-bs-keyboard="true" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
                     <div class="modal-dialog">
-                        <form >
+                        <form id="general_s_form">
                             <div class="modal-content">
                                 <div class="modal-header">
                                     <h1 class="modal-title fs-5">General Settings</h1>
                                 </div>
                                 <div class="modal-body">
                                     <div class="mb-3">
-                                        <label class="form-label">Site Title</label>
-                                        <input type="text" name="site_title" id="site_title_inp" class="form-control shadw-none">
+                                        <label class="form-label fw-bold">Site Title</label>
+                                        <input type="text" name="site_title" id="site_title_inp" class="form-control shadw-none" required>
                                     </div>
                                     <div class="mb-3 ">
-                                        <label class="form-label">About us</label>
-                                        <textarea name="site_about" id="site_about_inp" class="form-control shadow-none"  row="6"></textarea>
+                                        <label class="form-label fw-bold">About us</label>
+                                        <textarea name="site_about" id="site_about_inp" class="form-control shadow-none"  row="6" required></textarea>
                                     </div>
                                 </div>
                                 <div class="modal-footer">
                                     <button type="button" onclick="site_title.value = general_data.site_title, site_about.value = general_data.site_about" class="btn text-secondary shadow-none" data-bs-dismiss="modal">CANCEL</button>
-                                    <button type="button" onclick="update_general(site_title.value,site_about.value)" class="btn custom-bg text-white shadow-none">SUBMIT</button>
+                                    <button type="submit"  class="btn custom-bg text-white shadow-none">SUBMIT</button>
                                 </div>
                         </div>
                         </form>
@@ -88,22 +88,60 @@ adminlogin();
                 </div>
 
 
-
+            <!-- Contact details section  -->
+            <div class="card border-0 shadow mb-4">
+                <div class="card-body">
+                    <div class="d-flex align-items-center justify-content-between mb-3">
+                        <h5 class="card-title m-0">Contact Settings</h5>
+                        <button type="button" class="btn btn-dark btn-sm shadow-none" data-bs-toggle="modal" data-bs-target="#contact-settings">
+                            <i class="bi bi-pencil-square"></i> Edit
+                        </button>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="mb-4">
+                                <h6 class="card-subtitle mb-1 fw-bold">Address</h6>
+                                <p class="card-text" id="Address"></p>
+                            </div>
+                            <div class="mb-4">
+                                <h6 class="card-subtitle mb-1 fw-bold">Google map</h6>
+                                <p class="card-text" id="gmap"></p>
+                            </div>
+                            <div class="mb-4">
+                                <h6 class="card-subtitle mb-1 fw-bold">Phone number</h6>
+                                <p class="card-text" id="Address">
+                                <i class="bi bi-telephone-fill"></i> <span id="pn1"></span>
+                                </p>
+                                <p class="card-text" id="Address">
+                                    <i class="bi bi-telephone-fill"></i> <span id="pn2"></span>
+                                </p>
+                            </div>
+                            <div class="mb-4">
+                                <h6 class="card-subtitle mb-1 fw-bold">E-mail</h6>
+                                <p class="card-text" id="email"></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             </div>
         </div>
     </div>
 
 
+
+
+    
     <?php require('inc/script.php') ?>
     <script>
         let general_data;
-        
+        let general_s_form = document.getElementById('general_s_form');
+        let site_title_inp = document.getElementById("site_title_inp");
+        let site_about_inp = document.getElementById("site_about_inp");
 
         function get_general() {
             let site_title = document.getElementById("site_title");
             let site_about = document.getElementById("site_about");
-            let site_title_inp = document.getElementById("site_title_inp");
-            let site_about_inp = document.getElementById("site_about_inp");
 
             let shutdown_toggle = document.getElementById("shutdown-toggle");
 
@@ -131,6 +169,10 @@ adminlogin();
             
         }
 
+        general_s_form.addEventListener('submit', function(e){
+            e.preventDefault();
+            update_general(site_title_inp.value,site_about_inp.value)
+        })
 
         function update_general(site_title_val, site_about_val) {
             let xhr = new XMLHttpRequest();
